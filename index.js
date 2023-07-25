425bf3bbbef3ec2520a11e7bd277e55bca307e53
Shree Varshini a <138498938+ashreevarshini@users.noreply.github.com>
2023-07-26 01:06:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,97 +1,27 @@
-let name="shree";
-let age=19;
-let completedCollege=false
-let skills=["l","m"];
-console.log(name);
-console.log(age);
-console.log(completedCollege);
-console.log(skills);
-console.log(typeof age);
-console.log(typeof skills);
-console.log(typeof name);
-console.log("my name is "+name);
-console.log(`my age is ${age}`);
-alert("hello");
-const num1 = parseInt(prompt("enter num1"));
-const num2 = parseInt(prompt("enter num2"));
-result=num1+num2;
-alert(`result is  ${result}`);
-
-
-let name= prompt("enter name");
-alert(`hey ${name}`);
-const sampleText =['leo','lkg','kgf'];
-console.log(sampleText);
-console.log(sampleText.length);
-console.log(sampleText[2]);
-sampleText[2]="jawan";
-console.log(sampleText);
-sampleText.push("jailer");
-console.log(sampleText);
-sampleText.unshift("beast");
-console.log(sampleText);
-sampleText.pop()
-console.log(sampleText);
-sampleText.shift();
-console.log(sampleText);
-sampleText.splice(2,0,"beast");
-console.log(sampleText);
-
-let persons = [
-    { name: 'janani', age: 10, city: 'New York' },
-    { name: 'haritha', age: 20, city: 'Los Angeles' },
-    { name: 'priya', age: 30, city: 'Chicago' }
-  ];
-  persons.forEach((value,index) => {
-    if(value.age>=20){
-    console.log(value)
+const Arr=[94,67,15,22,83,38,67,49,30,15,82,52,13,73,76,92,65,95,29,38,83,9,1,48,32];
+function sumofeven(arr){
+    let sum=0;
+    for (let i=0;i<arr.length;i++){
+        if(arr[i]%2===0){
+            sum+=arr[i];
+        }
     }
-})
-
-  persons.forEach((value,index) => {
-    if(value.name.length>=6){
-    console.log(value)
+    return sum;    
+}
+const evensum=sumofeven(Arr);
+console.log("Sum of all even numbers:",evensum);
+console.log("Maximum number:",Math.max(...Arr));
+console.log("Minimum number:",Math.min(...Arr));
+console.log("Index of value of 48:",Arr.indexOf(48));
+function isascending(array){
+    for (let i=1;i<array.length;i++){
+        if (array[i]<array[i-1]){
+            return false;
+        }
     }
-})
- 
-let movies = [
-    { title: 'Inception', director: 'Christopher Nolan', year: 2010 },
-    { title: 'The Shawshank Redemption', director: 'Frank Darabont', year: 1994 },
-    { title: 'The Godfather', director: 'Francis Ford Coppola', year: 1972 }
-  ];
-   movies.forEach((value,index) => {
-    if(value.year>=1990){
-    console.log(value)
-    }
-})
-
-movies.forEach((value,index) => {
-    if(value.year==1994){
-    console.log(value)
-    }
-})
-
-let persons = [
-    { id:1,name: 'John', age: 25},
-    { id:2,name: 'Ja', age: 30 },
-    { id:3,name: 'Mike', age: 28 },
-  ];
-  persons.forEach((value,index) => {
-    if(value.name.length>=3){
-    console.log(value)
-    }
-  })
-
-const programminglanguages = [
-    { name: 'Javascriptn',type: 'cripting',year:1995 },
-    { name: 'Python',type: 'Scripting', year:1991  },
-    { name: 'Java',type: 'Compiled',year:1995  },
-    { name: 'C++', type: 'Compiled',year:1985  },
-  ];
-  programminglanguages.forEach((value,index) => {
-    if(value.year>=1990 && value.type =='Compiled'){
-    console.log(value)
-    }
-  })
-
+    return true;
+}
+console.log("Is array in ascending order:",isascending(Arr));
+const dup=Arr.filter((value,index,self)=>self.indexOf(value)===index);
+console.log("Array after removing duplicates:",dup);
 
